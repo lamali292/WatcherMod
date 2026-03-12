6b2ab0fa945963b39260d5d106acd9b35d02ae0f
Laurin Liebhart <l.liebhart@tu-braunschweig.de>
2026-03-12 11:47:13 +0100

    removed extra fear no evil i added for testing

--- src/WatcherMod/Patches/ExamplePatch.cs
+++ src/WatcherMod/Patches/ExamplePatch.cs
@@ -2,7 +2,6 @@ using HarmonyLib;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Unlocks;
-using WatcherMod.Models.Cards;
 
 namespace WatcherMod.Patches;
 
@@ -15,7 +14,7 @@ public class ExamplePatch
         //var watcherPool = ModelDb.CardPool<WatcherCardPool>();
         //var latestCards = watcherPool.AllCards.TakeLast(10);
 
-        __result.Deck.AddInternal(ModelDb.Card<FearNoEvil>().ToMutable());
+        //__result.Deck.AddInternal(ModelDb.Card<FearNoEvil>().ToMutable());
         //foreach (var card in latestCards) __result.Deck.AddInternal(card.ToMutable());
         //var customRelic = ModelDb.Relic<Melange>().ToMutable();
         //__result.AddRelicInternal(customRelic);
