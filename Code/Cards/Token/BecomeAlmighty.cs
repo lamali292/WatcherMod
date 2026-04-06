f9544db5e136dc8e414cacca38b0941f1ba32c58
Laurin Liebhart <l.liebhart@tu-braunschweig.de>
2026-04-06 17:47:54 +0200

    Refactored card and power logic by replacing `IScryable` and `IWishable` interfaces with unified hooks (`IOnScryed` and `WishableWatcherCard`) and abstract base classes. Simplified card actions and scry mechanics for consistency.

--- Code/Cards/Token/BecomeAlmighty.cs
+++ Code/Cards/Token/BecomeAlmighty.cs
@@ -4,25 +4,20 @@ using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models.CardPools;
 using MegaCrit.Sts2.Core.Models.Powers;
 using Watcher.Code.Abstract;
-using Watcher.Code.Cards.CardModels;
 
 namespace Watcher.Code.Cards.Token;
 
 [Pool(typeof(TokenCardPool))]
-public sealed class BecomeAlmighty : WatcherCardModel, IWishable
+public sealed class BecomeAlmighty : WishableWatcherCard
 {
     public BecomeAlmighty() : base(-1, CardType.Power, CardRarity.Token, TargetType.None)
     {
         WithPower<StrengthPower>(3, 1);
     }
 
-    public async Task OnWish(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    public override async Task OnWish(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         await CommonActions.ApplySelf<StrengthPower>(this);
     }
-
-    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
-    {
-        await OnWish(choiceContext, cardPlay);
-    }
+    
 }
--- Code/Interfaces/IWishable.cs
+++
@@ -1,9 +0,0 @@
-﻿using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-
-namespace Watcher.Code.Cards.CardModels;
-
-public interface IWishable
-{
-    Task OnWish(PlayerChoiceContext choiceContext, CardPlay cardPlay);
-}
