c7cc06916a821c199901e968b5c0410e353a6a94
lamali <lamali292@gmail.com>
2026-04-14 02:09:21 +0200

    Add harmony patch for `CombatState` hook listeners and update `Watcher` mod to v1.2.9.

--- Code/Events/WatcherSubscriber.cs
+++ Code/Events/WatcherSubscriber.cs
@@ -1,4 +1,5 @@
-﻿using MegaCrit.Sts2.Core.Combat;
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Modding;
 using MegaCrit.Sts2.Core.Models;
 using Watcher.Code.Core;
@@ -10,13 +11,31 @@ public class WatcherSubscriber
 {
     public static void Subscribe()
     {
-        ModHelper.SubscribeForCombatStateHooks(WatcherMainFile.ModId, CollectModels2);
+        //ModHelper.SubscribeForCombatStateHooks(WatcherMainFile.ModId, CollectModels2);
     }
 
-    private static IEnumerable<AbstractModel> CollectModels2(CombatState combatState)
+    
+    public static IEnumerable<AbstractModel> CollectModels2(CombatState combatState)
     {
         return combatState.Players
             .Select(WatcherModel.GetStanceModel)
             .Where(s => s is not NoStance);
+    }
+}
+
+[HarmonyPatch(typeof(CombatState), nameof(CombatState.IterateHookListeners))]
+public static class CombatStateHookListenersPatch
+{
+    static IEnumerable<AbstractModel> Postfix(
+        IEnumerable<AbstractModel> __result,
+        CombatState __instance)
+    {
+        foreach (var model in __result)
+            yield return model;
+
+        foreach (var stanceModel in WatcherSubscriber.CollectModels2(__instance))
+        {
+            yield return stanceModel;
+        }
     }
 }
