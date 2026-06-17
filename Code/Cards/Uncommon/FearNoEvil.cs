726e306c10c3f4875d354317a7b6fcaec38c186d
lamali <lamali292@gmail.com>
2026-06-17 18:15:30 +0200

    Simplify `FearNoEvil` attack intent check by consolidating null checks and logic.

--- Code/Cards/Uncommon/FearNoEvil.cs
+++ Code/Cards/Uncommon/FearNoEvil.cs
@@ -21,14 +21,9 @@ public sealed class FearNoEvil : WatcherCardModel
 
     protected override async Task OnPlay(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
-        if (cardPlay.Target == null) return;
-        var hasAttackIntent = false;
-
         // Check if the enemy intends to attack
-        if (cardPlay.Target.Monster != null)
-            hasAttackIntent = cardPlay.Target.Monster.NextMove.Intents
-                .Any(intent => intent is AttackIntent);
-
+        var hasAttackIntent = cardPlay.Target?.Monster?.NextMove.Intents
+            .Any(intent => intent.IntentType == IntentType.Attack) ?? false;
         await CommonActions.CardAttack(this, cardPlay).Execute(ctx);
         if (hasAttackIntent) await StanceCmd.EnterCalm(ctx, Owner, cardPlay.Card);
     }
