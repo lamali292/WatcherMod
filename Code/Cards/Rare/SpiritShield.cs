3bc51a2c194e3cead2de7bfa5dcaa45f25e0a123
lamali <lamali292@gmail.com>
2026-06-19 07:31:42 +0200

    Refactor `SpiritShield`: implement `WithCalculatedBlock`, update block calculation logic, and streamline `OnPlay` actions.

--- Code/Cards/Rare/SpiritShield.cs
+++ Code/Cards/Rare/SpiritShield.cs
@@ -1,8 +1,11 @@
 ﻿using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
 using Watcher.Code.Abstract;
 using Watcher.Code.Character;
 
@@ -13,17 +16,16 @@ public sealed class SpiritShield : WatcherCardModel
 {
     public SpiritShield() : base(2, CardType.Skill, CardRarity.Rare, TargetType.Self)
     {
-        WithBlock(3, 1);
+        WithCalculatedBlock(0, 3, Calc, ValueProp.Move, 0, 1);
     }
 
-    private static decimal Calc(Player player)
+    private static decimal Calc(CardModel cardModel, Creature? creature)
     {
-        return PileType.Hand.GetPile(player).Cards.Count;
+        return cardModel.Owner.PlayerCombatState?.Hand.Cards.Count(e => e != cardModel) ?? 0;
     }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block.IntValue * Calc(Owner), DynamicVars.Block.Props,
-            cardPlay);
+        await CommonActions.CardBlock(this, cardPlay);
     }
 }
